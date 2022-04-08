64a5800ff71f21ae853cd0565c94a804147da66d
Henning Blomqvist <74746184+hblomqvist@users.noreply.github.com>
2022-04-08 15:58:58 +0200

    refactor(eval): use ternary in `EvalOutputHandler`

--- src/lib/eval/output-handlers/EvalOutputHandler.ts
+++ src/lib/eval/output-handlers/EvalOutputHandler.ts
@@ -3,8 +3,6 @@ import type { EvalOutput, EvalPayload } from "../types";
 
 export abstract class EvalOutputHandler extends ChainableHandler<EvalPayload, EvalOutput> {
 	protected buildContent(body: string, message: string | undefined) {
-		if (message) return `${message}\n\n${body}`;
-
-		return body;
+		return message ? `${message}\n\n${body}` : body;
 	}
 }
