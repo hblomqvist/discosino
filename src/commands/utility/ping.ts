628d274b795c7364969057c24500af24665f3bab
Henning Blomqvist <74746184+hblomqvist@users.noreply.github.com>
2022-04-17 04:18:36 +0200

    feat(ping): add ping command

---
+++ src/commands/utility/ping.ts
@@ -0,0 +1,45 @@
+import { COMMAND_GUILD_IDS } from "#config";
+import { DiscosinoColor } from "#lib/constants";
+import { formatDurationShort } from "#util/time";
+import { ApplyOptions } from "@sapphire/decorators";
+import { ChatInputCommand, Command } from "@sapphire/framework";
+import { Message, MessageEmbed } from "discord.js";
+
+@ApplyOptions<ChatInputCommand.Options>({
+	description: "Measures bot latency."
+})
+export class UserCommand extends Command {
+	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+		registry.registerChatInputCommand(
+			(builder) =>
+				builder //
+					.setName(this.name)
+					.setDescription(this.description),
+			{
+				guildIds: COMMAND_GUILD_IDS,
+				idHints: ["965065317254574110"]
+			}
+		);
+	}
+
+	public override async chatInputRun(interaction: ChatInputCommand.Interaction) {
+		const message = (await interaction.deferReply({
+			ephemeral: true,
+			fetchReply: true
+		})) as Message;
+
+		const rtt = message.createdTimestamp - interaction.createdTimestamp;
+		const { ping } = this.container.client.ws;
+
+		const embed = new MessageEmbed() //
+			.setColor(DiscosinoColor.Primary)
+			.addField("Round Trip", this.format(rtt))
+			.addField("Client", this.format(ping));
+
+		return interaction.editReply({ embeds: [embed] });
+	}
+
+	private format(ms: number) {
+		return `\`⏱️ ${formatDurationShort(ms, 0)}\``;
+	}
+}
