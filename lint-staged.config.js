1c8f46b479e107e3ded685c77a69938c46428ac3
Henning Blomqvist <74746184+hblomqvist@users.noreply.github.com>
2022-05-14 14:02:08 +0200

    chore: lint-staged should work now

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,14 @@
+import { ESLint } from "eslint";
+
+async function removeIgnoredFiles(files) {
+	const eslint = new ESLint();
+	const ignoreList = await Promise.all(files.map((file) => eslint.isPathIgnored(file)));
+	const filteredFiles = files.filter((value, index) => !ignoreList[index]);
+
+	return filteredFiles.join(" ");
+}
+
 export default {
-	"**/*.ts": () => ["yarn typecheck", "eslint --fix --no-error-on-unmatched-pattern"]
+	"**/*.ts": () => "yarn typecheck",
+	"**/*.{ts,js}": async (files) => `eslint --fix ${await removeIgnoredFiles(files)}`
 };
