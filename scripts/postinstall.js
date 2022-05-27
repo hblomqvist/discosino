26049c982f7a6b1f572cde28315e75f5eb47fae0
Henning Blomqvist <74746184+hblomqvist@users.noreply.github.com>
2022-05-27 20:04:16 +0200

    chore(husky): reconfigure hooks installation

--- scripts/postinstall.js
+++
@@ -1,8 +0,0 @@
-if (process.env.CI !== "true") {
-	try {
-		const husky = await import("husky");
-		husky.install();
-	} catch (error) {
-		if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
-	}
-}
