a0b1fe33a327a483396caa113387849d4a9a0758
Henning Blomqvist <74746184+hblomqvist@users.noreply.github.com>
2022-07-08 20:32:01 +0200

    chore(prisma): nuke migrations and start over

--- prisma/migrations/20220708183121_init/migration.sql
+++ prisma/migrations/20220708183121_init/migration.sql
@@ -9,15 +9,15 @@ CREATE TABLE "members" (
 
 -- CreateTable
 CREATE TABLE "accounts" (
-    "owner_id" INTEGER NOT NULL,
-    "money_amount" TEXT NOT NULL DEFAULT E'0',
-    "token_amount" TEXT NOT NULL DEFAULT E'0',
+    "id" INTEGER NOT NULL,
+    "money_string" TEXT NOT NULL DEFAULT '0',
+    "token_string" TEXT NOT NULL DEFAULT '0',
 
-    CONSTRAINT "accounts_pkey" PRIMARY KEY ("owner_id")
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "members_guild_id_user_id_key" ON "members"("guild_id", "user_id");
 
 -- AddForeignKey
-ALTER TABLE "accounts" ADD CONSTRAINT "accounts_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "members"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_id_fkey" FOREIGN KEY ("id") REFERENCES "members"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
