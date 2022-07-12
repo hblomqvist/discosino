f1954f1ebfa02b34c57351db80b315fc1f468f5e
Henning Blomqvist <74746184+hblomqvist@users.noreply.github.com>
2022-07-13 00:20:48 +0200

    refactor(database): future-proof to hopefully avoid ever writing SQL

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,37 +1,40 @@
 import type { MemberIdentifier } from "#util/types";
-import type { Member } from "@prisma/client";
+import type { Guild, User } from "@prisma/client";
 import { container } from "@sapphire/framework";
-
-export function findMember(identifier: MemberIdentifier): Promise<Member> {
-	return container.database.member.findUniqueOrThrow({
-		where: {
-			guildId_userId: identifier
-		}
-	});
-}
-
-export async function findAccount(identifier: MemberIdentifier) {
-	const { id } = await findMember(identifier);
-
-	return container.database.account.findUniqueOrThrow({
-		where: { id }
-	});
-}
-
-export function ensureMember(identifier: MemberIdentifier): Promise<Member> {
-	return container.database.member.upsert({
-		where: {
-			guildId_userId: identifier
-		},
-		create: identifier,
-		update: {}
-	});
-}
 
 export async function ensureAccount(identifier: MemberIdentifier) {
 	const { id } = await ensureMember(identifier);
 
 	return container.database.account.upsert({
+		where: { id },
+		create: { id },
+		update: {}
+	});
+}
+
+export function ensureGuild(id: string): Promise<Guild> {
+	return container.database.guild.upsert({
+		where: { id },
+		create: { id },
+		update: {}
+	});
+}
+
+export async function ensureMember({ guildId, userId }: MemberIdentifier) {
+	await ensureGuild(guildId);
+	await ensureUser(userId);
+
+	return container.database.member.upsert({
+		where: {
+			guildId_userId: { guildId, userId }
+		},
+		create: { guildId, userId },
+		update: {}
+	});
+}
+
+export function ensureUser(id: string): Promise<User> {
+	return container.database.user.upsert({
 		where: { id },
 		create: { id },
 		update: {}
--- src/lib/economy.ts
+++ src/lib/economy.ts
@@ -1,20 +1,43 @@
 import type { MemberIdentifier } from "#util/types";
-import { findAccount } from "./database";
+import { container } from "@sapphire/framework";
+import { ensureAccount } from "./database";
 
 export async function getBalance(identifier: MemberIdentifier): Promise<AccountBalance> {
-	try {
-		const { moneyString, tokenString } = await findAccount(identifier);
+	const account = await container.database.member
+		.findUnique({
+			where: {
+				guildId_userId: identifier
+			}
+		})
+		.account();
 
-		return {
-			moneyAmount: BigInt(moneyString),
-			tokenAmount: BigInt(tokenString)
-		};
-	} catch {
-		return {
-			moneyAmount: 0n,
-			tokenAmount: 0n
-		};
-	}
+	return {
+		moneyAmount: BigInt(account?.moneyString ?? "0"),
+		tokenAmount: BigInt(account?.tokenString ?? "0")
+	};
+}
+
+export async function setBalance(identifier: MemberIdentifier, { moneyAmount, tokenAmount }: Partial<AccountBalance>) {
+	const { id } = await ensureAccount(identifier);
+
+	await container.database.account.update({
+		where: { id },
+		data: {
+			moneyString: moneyAmount?.toString(),
+			tokenString: tokenAmount?.toString()
+		}
+	});
+}
+
+export async function incrementBalance(identifier: MemberIdentifier, increments: Partial<AccountBalance>) {
+	const { moneyAmount, tokenAmount } = await getBalance(identifier);
+
+	const updatedBalance = {
+		moneyAmount: moneyAmount + (increments.moneyAmount ?? 0n),
+		tokenAmount: tokenAmount + (increments.tokenAmount ?? 0n)
+	};
+
+	await setBalance(identifier, updatedBalance);
 }
 
 interface AccountBalance {
