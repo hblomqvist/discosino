394bc441b3994527b092cfe98b040af19e95777b
Henning Blomqvist <74746184+hblomqvist@users.noreply.github.com>
2022-07-15 20:49:11 +0200

    feat(env): improve parsing of env vars

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,27 @@
 import { LogLevel } from "@sapphire/framework";
 import { ClientOptions, Intents } from "discord.js";
-import { cleanEnv, makeValidator, str, url } from "envalid";
+import { cleanEnv, EnvMissingError, makeValidator, str, url } from "envalid";
 
-const strArray = makeValidator((value) => value.split(" "));
+const strArray = makeValidator((value) => value.split(",").map((value) => value.trim()));
+const strNotEmpty = makeValidator((value) => {
+	if (!value) throw new EnvMissingError();
+
+	return value;
+});
 
 export const ENV = cleanEnv(process.env, {
 	NODE_ENV: str({
 		choices: ["development", "production"],
 		default: "development"
 	}),
-	CLIENT_ACTIVITY_NAME: str({ default: "" }),
-	CLIENT_ACTIVITY_TYPE: str({
+	ACTIVITY_NAME: str({ default: "" }),
+	ACTIVITY_TYPE: str({
 		choices: ["PLAYING", "STREAMING", "LISTENING", "WATCHING", "COMPETING"],
 		default: "PLAYING"
 	}),
 	DEVELOPER_IDS: strArray({ default: [] }),
 	DATABASE_URL: url(),
-	DISCORD_TOKEN: str(),
+	DISCORD_TOKEN: strNotEmpty(),
 	PASTE_GG_TOKEN: str({ default: "" })
 });
 
@@ -27,6 +32,6 @@ export const CLIENT_OPTIONS: ClientOptions = {
 	},
 	partials: ["CHANNEL"],
 	presence: {
-		activities: [{ name: ENV.CLIENT_ACTIVITY_NAME, type: ENV.CLIENT_ACTIVITY_TYPE }]
+		activities: [{ name: ENV.ACTIVITY_NAME, type: ENV.ACTIVITY_TYPE }]
 	}
 };
