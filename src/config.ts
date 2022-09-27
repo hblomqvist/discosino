3d044f51d41ccf24f392d09775fb21b0c4032eb3
Henning Blomqvist <74746184+hblomqvist@users.noreply.github.com>
2022-09-27 02:10:44 +0200

    refactor(config): export `SANITIZER_SUFFIXES` as const

--- src/config.ts
+++ src/config.ts
@@ -36,7 +36,7 @@ export const CLIENT_OPTIONS: ClientOptions = {
 	}
 };
 
-export const SANITIZER_SUFFIXES = ["DATABASE_URL", "TOKEN"];
+export const SANITIZER_SUFFIXES = ["DATABASE_URL", "TOKEN"] as const;
 
 export const enum DiscosinoColor {
 	Primary = 0x5865f2,
