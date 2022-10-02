fc1fe5d244d31286528fb8cf250cc23ac3ce5ea7
Henning Blomqvist <74746184+hblomqvist@users.noreply.github.com>
2022-10-02 18:31:31 +0200

    refactor(eval): remove unused method

--- src/lib/eval/output-handlers/EvalOutputHandler.ts
+++ src/lib/eval/output-handlers/EvalOutputHandler.ts
@@ -1,24 +1,8 @@
 import { ChainableHandler } from '#util/structures';
-import { format, Options } from 'prettier';
 import type { EvalOutput, EvalPayload } from '../types';
 
 export abstract class EvalOutputHandler extends ChainableHandler<EvalPayload, EvalOutput> {
 	protected buildContent(body: string, message: string | undefined) {
 		return message ? `${message}\n\n${body}` : body;
-	}
-
-	protected formatCode(code: string, options?: Options) {
-		try {
-			const formattedCode = format(code, {
-				parser: 'babel',
-				singleQuote: true,
-				trailingComma: 'none',
-				...options
-			}).replace(/\n$/, '');
-
-			return formattedCode || code;
-		} catch {
-			return code;
-		}
 	}
 }
