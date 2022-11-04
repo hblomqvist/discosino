6815ae5e3822822b65f20be2b0a72f381ca02936
Henning Blomqvist <74746184+hblomqvist@users.noreply.github.com>
2022-11-04 01:02:51 +0100

    feat(client): log to console on login

---
+++ src/listeners/ClientReady.ts
@@ -0,0 +1,12 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Events, Listener } from '@sapphire/framework';
+import type { Client } from 'discord.js';
+
+@ApplyOptions<Listener.Options>({
+	event: Events.ClientReady
+})
+export class UserListener extends Listener<typeof Events.ClientReady> {
+	public override run(client: Client) {
+		this.container.logger.info(`Client: Successfully logged in to Discord as ${client.user!.tag} (${client.user!.id})`);
+	}
+}
